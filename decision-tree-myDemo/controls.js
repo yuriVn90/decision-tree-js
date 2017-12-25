c8a74aad8ad0af6e7e2b1a26ada72985a98ab614
Yuri Vainstain <yuri.vn@gmail.com>
2017-12-25 13:18:57 +0200

    changed attributes values

--- decision-tree-myDemo/controls.js
+++ decision-tree-myDemo/controls.js
@@ -2,15 +2,15 @@ function init() {
 
     // Training set
     var data =
-        [{person: 'Homer', hairLength: 0, weight: 250, age: 36, sex: 'male'},
-            {person: 'Marge', hairLength: 10, weight: 150, age: 34, sex: 'female'},
-            {person: 'Bart', hairLength: 2, weight: 90, age: 10, sex: 'male'},
-            {person: 'Lisa', hairLength: 6, weight: 78, age: 8, sex: 'female'},
-            {person: 'Maggie', hairLength: 4, weight: 20, age: 1, sex: 'female'},
-            {person: 'Abe', hairLength: 1, weight: 170, age: 70, sex: 'male'},
-            {person: 'Selma', hairLength: 8, weight: 160, age: 41, sex: 'female'},
-            {person: 'Otto', hairLength: 10, weight: 180, age: 38, sex: 'male'},
-            {person: 'Krusty', hairLength: 6, weight: 200, age: 45, sex: 'male'}];
+        [{person: 'Homer', hairLength: 0, weight: 113, age: 36, sex: 'male'},
+            {person: 'Marge', hairLength: 18, weight: 68, age: 34, sex: 'female'},
+            {person: 'Bart', hairLength: 4, weight: 40, age: 10, sex: 'male'},
+            {person: 'Lisa', hairLength: 10, weight: 35, age: 8, sex: 'female'},
+            {person: 'Maggie', hairLength: 7, weight: 38, age: 1, sex: 'female'},
+            {person: 'Abe', hairLength: 2, weight: 77, age: 70, sex: 'male'},
+            {person: 'Selma', hairLength: 14, weight: 72, age: 41, sex: 'female'},
+            {person: 'Otto', hairLength: 18, weight: 81, age: 38, sex: 'male'},
+            {person: 'Krusty', hairLength: 10, weight: 90, age: 45, sex: 'male'}];
 
     // Configuration
     var config = {
@@ -23,7 +23,7 @@ function init() {
     var decisionTree = new dt.DecisionTree(config);
 
     // Testing Decision Tree
-    var comic = {person: 'Comic guy', hairLength: 8, weight: 290, age: 38};
+    var comic = {person: 'Comic guy', hairLength: 14, weight: 131, age: 38};
 
     var decisionTreePrediction = decisionTree.predict(comic);
 
